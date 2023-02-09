fadc447918436707ffb745d564e5505cad7df273
tsule27 <104784854+tsule27@users.noreply.github.com>
2023-02-09 12:33:01 -0500

    Added fahrenheit to WeatherTemperature

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  return (
-    <div className="WeatherTemperature d-flex align-items-top">
-      <h4 className="temperature">{Math.round(props.celsius)}</h4>
-      <span className="units">
-        <a href="/" className="active">
-          °C
-        </a>{" "}
-        | <a href="/">°F</a>
-      </span>
-    </div>
-  );
+  const [unit, setUnit] = useState("celsius");
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahreheit");
+  }
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsuis");
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div className="WeatherTemperature d-flex align-items-top">
+        <h4 className="temperature">{Math.round(props.celsius)}</h4>
+        <span className="units">
+          °C |{" "}
+          <a href="/" onClick={showFahrenheit}>
+            °F
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    let fahrenheit = (props.celsius * 9) / 5 + 32;
+    return (
+      <div className="WeatherTemperature d-flex align-items-top">
+        <h4 className="temperature">{Math.round(fahrenheit)}</h4>
+        <span className="units">
+          <a href="/" onClick={showCelsius}>
+            °C
+          </a>{" "}
+          | °F
+        </span>
+      </div>
+    );
+  }
 }
